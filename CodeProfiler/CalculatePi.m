ed182e743ab5b69ce817b5c4341b9a5948da99a7
Paolo <paolo.bardella@gmail.com>
2019-01-23 09:48:09 +0100

    first run without plot

--- CodeProfiler/CalculatePi.m
+++ CodeProfiler/CalculatePi.m
@@ -1,18 +1,15 @@
 function Pi=CalculatePi
-NumSamples=10000;
-R=0.5; 
-Inside=0;
-figure; 
-hold on;
+NumSamples=10e6;
+R=0.5; Inside=0;
+%figure;hold on;
 for k=1:NumSamples
     x=rand()-R;
     y=rand()-R;
     if sqrt(x*x+y*y)<=R
         Inside=Inside+1;
-        plot(x,y,'ko');
+        %plot(x,y,'ko');
     else
-        plot(x,y,'b.');
+        %plot(x,y,'b.');
     end
-    
 end
 Pi=4*Inside/NumSamples;
