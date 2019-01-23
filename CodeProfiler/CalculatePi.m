1e9b70b9cf2af16f3ac1ffb8770da3692e6166a8
Paolo <paolo.bardella@gmail.com>
2019-01-23 09:52:05 +0100

    Rand vectorized

--- CodeProfiler/CalculatePi.m
+++ CodeProfiler/CalculatePi.m
@@ -2,10 +2,10 @@ function Pi=CalculatePi
 NumSamples=10e6;
 R=0.5; Inside=0;
 %figure;hold on;
+x=rand(NumSamples,1)-R;
+y=rand(NumSamples,1)-R;
 for k=1:NumSamples
-    x=rand()-R;
-    y=rand()-R;
-    if sqrt(x*x+y*y)<=R
+    if sqrt(x(k)*x(k)+y(k)*y(k))<=R
         Inside=Inside+1;
         %plot(x,y,'ko');
     else
